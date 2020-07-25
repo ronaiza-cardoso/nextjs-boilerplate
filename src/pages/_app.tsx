cb60af365783572f2d48de6a10e6f5338211abc9
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-07-25 14:54:42 +0100

    chore(PWA): settingup offline support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Advanced React - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="a simple project to work with TypeScript, React, Nextjs and Styled Components"
